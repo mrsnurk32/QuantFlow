bbde75be3e3f1fe4637adc281316b560fecb5cdc
Nick <nickbelik@mail.ru>
2021-01-07 02:38:04 +0300

    1. FileCheck class modified: 	-check_files method added

--- datamanager/file_checker.py
+++ datamanager/file_checker.py
@@ -3,15 +3,33 @@ V 1.0
 This module check the file system and creates files if missing
 """
 
+import os
+
 class DBChecker:
     #checks if database exists
-
-
-class CreateDB:
-    #creates DB
+    pass
 
 
 class FileCheck:
-    #checks for missing folies outside directory
+
+    """
+    1. Checks for database directory
+    2. Creates one if missing
+    """
+    
+    DIRECTORIES = (
+        'asset_data',
+    )
+
+    @property
+    def check_files(self):
+
+        file_list = os.listdir()
+        for _dir in self.DIRECTORIES:
+            if _dir not in file_list:
+                print(f'created {_dir}')
+                os.mkdir(_dir)
+            
+
 
 
