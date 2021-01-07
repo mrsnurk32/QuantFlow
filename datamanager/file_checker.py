cadcbe30003249b8a39b4533c33ca780dcc6623c
Nick <nickbelik@mail.ru>
2021-01-07 17:40:50 +0300

    check if ticker in db

--- datamanager/file_checker.py
+++ datamanager/file_checker.py
@@ -5,9 +5,30 @@ This module check the file system and creates files if missing
 
 import os
 
-class DBChecker:
-    #checks if database exists
-    pass
+class DB_Checker:
+    #checks for asset data in db
+
+    @staticmethod
+    def get_update(conn, ticker):
+
+        """
+            The function returns true if asset exists in db to initialize update
+        if data is not present in db it will return false to initialize download method 
+        """
+            
+        table_lst = conn.execute(
+            "SELECT name FROM sqlite_master WHERE type='table';"
+        )
+
+        table_lst = [i[0] for i in table_lst.fetchall() if i[0] != 'stock_info']
+        table_lst = [i.split('_')[0] for i in table_lst if '1h' in i]
+
+        if ticker in table_lst:
+            return True
+
+        else:
+            return False
+        
 
 
 class FileCheck:
