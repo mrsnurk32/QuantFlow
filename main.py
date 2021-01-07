c58be8769431ae58383c8b1a7d8c50d95ec702bd
Nick <nickbelik@mail.ru>
2021-01-07 16:53:28 +0300

    gets ticker list

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ import os
 from datamanager.database_manager import Connector
 from datamanager.database_manager import StockConnector
 
+from datamanager.modules.tickers import RussianStockTickers
 
 
 if __name__ == "__main__":
 
     stock_path = StockConnector().get_path
 
+    ticker_list = RussianStockTickers().get_tickers
+
     with Connector(stock_path) as stock_conn:
         print('Hello world')
+        print(ticker_list)
 
     
     
