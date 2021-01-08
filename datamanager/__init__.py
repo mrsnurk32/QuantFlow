63f51f9265d82b5a6fce51024f46a049ff960947
Nick <nickbelik@mail.ru>
2021-01-09 01:49:39 +0300

    Modules added: DataChecker, Verify Data

--- datamanager/__init__.py
+++ datamanager/__init__.py
@@ -1,9 +1,9 @@
 from .database_manager import Connector
 from .database_manager import StockConnector
 
-from .download_asset import DownloadAsset
+from .download_asset import DownloadAsset, VerifyData
 from .update_asset import UpdateAsset
 
-from .file_checker import DB_Checker
+from .file_checker import DB_Checker, DataChecker
 
 from .modules import *
