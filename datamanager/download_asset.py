ff45ab922b846715fedf50a6943fcbf10ab46253
Nick <nickbelik@mail.ru>
2021-01-09 01:52:56 +0300

    tf > timeframe, new class added to verify data

--- datamanager/download_asset.py
+++ datamanager/download_asset.py
@@ -22,10 +22,10 @@ class DownloadAsset(AssetConfig):
         return mt5.copy_rates_from(ticker, mt_t_frame, utc_from, rows)
 
     
-    async def download_mt_asset(self,ticker, tf, conn, rows = 50000):
+    async def download_mt_asset(self,ticker, time_frame, conn, rows = 50000):
 
         frame_header, mt_t_frame,\
-            timezone = self.time_frame(tf)
+            timezone = self.time_frame(time_frame)
             
         ymd = dt.today()
         
@@ -46,3 +46,30 @@ class DownloadAsset(AssetConfig):
         ticker = ticker + '_' + frame_header
         
         rates_frame.to_sql(name=ticker,con = conn,index=False)
+        print(f'Downloaded {ticker} ({time_frame})')
+
+
+class VerifyData(DownloadAsset):
+
+    def verify_data(self,ticker, time_frame, rows = 500):
+
+        frame_header, mt_t_frame,\
+        timezone = self.time_frame(time_frame)
+            
+        ymd = dt.today()
+        
+        if WeekDays.trading_day():
+
+           delta = timedelta(days = 1)
+           ymd = (ymd - delta)
+
+        utc_from = dt(ymd.year, ymd.month, ymd.day, tzinfo=timezone)
+       
+        rates = mt5.copy_rates_from(ticker, mt_t_frame, utc_from, rows)
+        rates_frame = pd.DataFrame(rates)
+        rates_frame['time']=pd.to_datetime(rates_frame['time'], unit='s')
+
+        if len(rates_frame) < 1:
+            return "Check frame data ticker:{}".format(ticker)
+
+        return rates_frame
