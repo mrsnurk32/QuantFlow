a42eecf4968aaca9d6c52409775aa40614c5d948
mrsnurk32 <nickbelik@mail.ru>
2021-01-16 16:26:06 +0300

    mt_uploader added

--- datamanager/__init__.py
+++ datamanager/__init__.py
@@ -6,3 +6,4 @@ from data_base_manager import Connector, MT_MOEX_Stocks
 from data_base_manager import commit_handler
 
 # from data_uploader import mt_from, mt_interval
+from .datauploader import mt_uploader
