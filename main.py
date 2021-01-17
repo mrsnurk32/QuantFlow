463dbf4f16031622a24713454b569ffa1c5c1097
mrsnurk32 <nickbelik@mail.ru>
2021-01-18 00:29:11 +0300

    new data upload algo

--- main.py
+++ main.py
@@ -5,14 +5,20 @@
 """
 import os
 import asyncio
-
-from datamanager import Connector, StockConnector, \
-    DownloadAsset ,UpdateAsset, DB_Checker, AssetConfig, \
-        DataChecker
-
-from datamanager import RussianStockTickers
-
 import MetaTrader5 as mt5
+import pandas as pd
+
+#connection handlers
+from datamanager import connector, stock_path
+
+#db
+from datamanager import check_for_update
+
+#ticker data
+from datamanager import RussianStockTickers, time_frames
+
+#metatrader related modules
+from datamanager import mt_from, mt_interval, commit_data
 
 import time
 
@@ -20,81 +26,117 @@ from datetime import datetime as dt
 from datetime import timedelta
 
 
-if __name__ == "__main__":
+def update():
 
     if not mt5.initialize():
         print("initialize() failed, error code =",mt5.last_error())
         quit()
 
-    stock_path = StockConnector().get_path
-
-    ticker_list = RussianStockTickers().get_tickers
-
-    time_frames = AssetConfig().time_frames
     
-    with Connector(stock_path) as stock_conn:
+    with connector as stock_conn:
+        print('ok')
 
-        while True:
-            downloadloop = asyncio.get_event_loop()
-            tasks = list()
+        #get list of approved tickers
+        tickers = RussianStockTickers().custom_ticker_list
 
-            for ticker in ticker_list:
+        tasks = check_for_update(conn = stock_conn, tickers = tickers)
+        
+        commit_list = list()
+        
+        for ticker in tasks['download']:
+            ticker, time_frame = ticker.split('_')[0],ticker.split('_')[1]
+            commit_list.append((mt_from(ticker, time_frame),'download',ticker,time_frame))
 
-                for tf in time_frames:
 
-                    if DB_Checker.get_update(conn = stock_conn, ticker = ticker, time_frame = tf):
+        for ticker in tasks['update']:
+            ticker, time_frame = ticker.split('_')[0],ticker.split('_')[1]
+            
+            # commit_list.append((mt_from(ticker, time_frame)))
+            result = mt_interval(ticker, time_frame, stock_conn)
+            if len(result) > 1:
+                print('upd')
+                commit_list.append((result ,'update',ticker, time_frame))
 
-                        tasks.append(
-                            downloadloop.create_task(
-                                UpdateAsset().update_mt_asset(
-                                    ticker = ticker, time_frame = tf, conn = stock_conn
-                                )   
-                            )
-                        )
+        commit_data(stock_conn,commit_list)
+
+        #check data
+            
+if __name__ == "__main__":
+    
+    # import cProfile, pstats
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+
+    update()
+    # profiler.disable()
+    # stats = pstats.Stats(profiler).sort_stats('cumtime')
+    # stats.print_stats()
+
+            #commit
+            #check
+
+
+    #     while True:
+    #         downloadloop = asyncio.get_event_loop()
+    #         tasks = list()
+
+    #         for ticker in ticker_list:
+
+    #             for tf in time_frames:
+
+    #                 if DB_Checker.get_update(conn = stock_conn, ticker = ticker, time_frame = tf):
+
+    #                     tasks.append(
+    #                         downloadloop.create_task(
+    #                             UpdateAsset().update_mt_asset(
+    #                                 ticker = ticker, time_frame = tf, conn = stock_conn
+    #                             )   
+    #                         )
+    #                     )
                         
 
-                    else:
+    #                 else:
 
-                        tasks.append(
-                            downloadloop.create_task(
-                                DownloadAsset().download_mt_asset(
-                                    ticker = ticker, time_frame = tf, conn = stock_conn
-                                )   
-                            )
-                        )
+    #                     tasks.append(
+    #                         downloadloop.create_task(
+    #                             DownloadAsset().download_mt_asset(
+    #                                 ticker = ticker, time_frame = tf, conn = stock_conn
+    #                             )   
+    #                         )
+    #                     )
             
-            if len(tasks) > 0:
-                wait_tasks = asyncio.wait(tasks)
-                downloadloop.run_until_complete(wait_tasks)
-                downloadloop.close()
+    #         if len(tasks) > 0:
+    #             wait_tasks = asyncio.wait(tasks)
+    #             downloadloop.run_until_complete(wait_tasks)
+    #             downloadloop.close()
 
-            now = dt.now()
-            next_update = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours = 1)
-            sleep_time = (next_update - now).total_seconds()
+    #         now = dt.now()
+    #         next_update = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours = 1)
+    #         sleep_time = (next_update - now).total_seconds()
 
-            if dt.now().hour > 8:
-                time.sleep(sleep_time)
-                continue
+    #         if dt.now().hour > 8:
+    #             time.sleep(sleep_time)
+    #             continue
 
-            checker = DataChecker()
-            print('Checking Data!')
-            err_list = list()
+    #         checker = DataChecker()
+    #         print('Checking Data!')
+    #         err_list = list()
 
-            for ticker in ticker_list:
+    #         for ticker in ticker_list:
 
-                for tf in time_frames:
+    #             for tf in time_frames:
 
-                    result = checker.init_check(
-                        conn = stock_conn, ticker = ticker, time_frame = tf
-                        )
+    #                 result = checker.init_check(
+    #                     conn = stock_conn, ticker = ticker, time_frame = tf
+    #                     )
                     
-                    if result is False:
-                        err_list.append(ticker + '_' + tf)
+    #                 if result is False:
+    #                     err_list.append(ticker + '_' + tf)
 
 
-                    print(ticker,tf, result)
-            print('error list')
-            print(err_list)
-            print(f'seconds {sleep_time} until next update')
+    #                 print(ticker,tf, result)
+    #         print('error list')
+    #         print(err_list)
+    #         print(f'seconds {sleep_time} until next update')
 
-            time.sleep(sleep_time)
+    #         time.sleep(sleep_time)
