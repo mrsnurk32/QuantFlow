350627d42b55f2079f142cceae7b4ba0efbf269f
mrsnurk32 <nickbelik@mail.ru>
2021-01-18 00:29:59 +0300

    code improvement

--- datamanager/modules/asset_config.py
+++ datamanager/modules/asset_config.py
@@ -1,10 +1,12 @@
 import MetaTrader5 as mt5
 import pytz
 
+__all__ = ['asset_config', 'time_frames']
+
+
 class AssetConfig:
 
     '''
-    V 2.1
     AssetConfig class sets time zone and return config for timeframe
     '''
 
@@ -31,20 +33,43 @@ class AssetConfig:
         return self.TIME_FRAMES.keys()
 
     
-    def get_upload_interval(self, time_frame):
+    def get_upload_interval(self, time_frame:str) -> dict:
+        """
+            The method returns time interval for datetime
+            args:
+                1. time_frame: a string type object, that indicates time frame of time series
+            returns:
+                1. dict with time interval {time_unit:n}
+        """
         return self.TIME_INTERVAL[time_frame]
 
 
-    def time_frame(self,time_frame, update = False):
+    def __call__(self,time_frame:str, update = False) -> dict:
 
-        # assert time_frame not in self.TIME_FRAMES
+        """
+            This class method validates the time_frame, and returns parameters for 'update' and 'download' functions
+            args:
+                1. time_frame: a string type object, that indicates time frame of time series
+                2. update: a boolean, that clarify wether the call method needs to return download config data or update config data
+            
+                if update is False, the class method returns config data needed for download function:
+                    time_frame, mt.TIMEFRAME, timezone
+                otherwise the class method returns config data needed for updated function:
+                    time_frame, mt.TIMEFRAME, timezone, upload_interval
+        """
 
-        if update == False:
-            return time_frame, self.TIME_FRAMES[time_frame], self.timezone
+        assert time_frame in self.TIME_FRAMES, 'Such time frame is not supported'
 
-        else:
+        result = dict(
+            time_frame = time_frame, mt_time_frame = self.TIME_FRAMES[time_frame], timezone = self.timezone
+        )
 
-            return time_frame, self.TIME_FRAMES[time_frame], self.timezone,\
-                self.get_upload_interval(time_frame)
+        if update:
+            result |= {'interval':self.get_upload_interval(time_frame)}
+                       
+        return result
+                
 
 
+asset_config = AssetConfig()
+time_frames = AssetConfig().time_frames
